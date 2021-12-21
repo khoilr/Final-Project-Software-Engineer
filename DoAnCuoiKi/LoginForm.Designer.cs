d971a16f99552f6555326762981585fac6e52940
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2021-12-21 17:44:32 +0700

    big bang

---
+++ DoAnCuoiKi/LoginForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DoAnCuoiKi
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(668, 212);
+            this.panel1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(477, 140);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(172, 58);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.passBox);
+            this.panel3.Controls.Add(this.passLabel);
+            this.panel3.Location = new System.Drawing.Point(3, 66);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(655, 68);
+            this.panel3.TabIndex = 1;
+            // 
+            // passBox
+            // 
+            this.passBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passBox.Location = new System.Drawing.Point(259, 7);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(387, 44);
+            this.passBox.TabIndex = 1;
+            this.passBox.UseSystemPasswordChar = true;
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passLabel.Location = new System.Drawing.Point(3, 10);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(158, 37);
+            this.passLabel.TabIndex = 0;
+            this.passLabel.Text = "Password";
+            this.passLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.userBox);
+            this.panel2.Controls.Add(this.userLabel);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(655, 57);
+            this.panel2.TabIndex = 0;
+            // 
+            // userBox
+            // 
+            this.userBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userBox.Location = new System.Drawing.Point(259, 7);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(387, 44);
+            this.userBox.TabIndex = 1;
+            this.userBox.TextChanged += new System.EventHandler(this.userBox_TextChanged);
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(3, 10);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(164, 37);
+            this.userLabel.TabIndex = 0;
+            this.userLabel.Text = "Username";
+            this.userLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 236);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.Text = "Login - Storehouse Management";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
